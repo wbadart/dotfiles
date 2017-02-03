23531f0cd645360bad685bb2f11c6c3e8d48da25
Will Badart <wbadart@nd.edu>
2017-02-02 21:25:58 -0500

    Added init script

---
+++ init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+##
+# init.sh
+#
+# Links dotfiles to their proper locations
+# in the user's home directory.
+#
+# Will Badart
+# created: FEB 2017
+##
+
+ln -s $PWD/.vim ~/.vim
+ln -s $PWD/.vimrc ~/.vimrc
+
