53b7bbb4ed0552243f0e9b56024bed2fb1d380aa
Will Badart <wbadart@live.com>
2018-01-19 16:45:19 -0500

    Fixed history scrolling

--- zsh/zshresources/completion.zsh
+++ zsh/zshresources/completion.zsh
@@ -1,4 +1,8 @@
+autoload -U history-search-end
+zle -N history-beginning-search-backward-end history-search-end
+zle -N history-beginning-search-forward-end history-search-end
+
 zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 bindkey "^R" history-incremental-search-backward
-bindkey "^[[A" history-beginning-search-backward
-bindkey "^[[B" history-beginning-search-forward
+bindkey "^[[A" history-beginning-search-backward-end
+bindkey "^[[B" history-beginning-search-forward-end
