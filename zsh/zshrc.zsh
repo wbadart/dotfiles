1261c6769901a6fcc05de2b8b6633abc27c03b9c
Will Badart <wbadart@live.com>
2018-01-19 16:49:10 -0500

    Moved completion colors to completion module

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -7,24 +7,7 @@
 # created: JAN 2018
 ##
 
-setopt PROMPT_SUBST
 ZSH_DIR="$HOME/.zsh"
-
-# Do menu-driven completion.
-zstyle ':completion:*' menu select
-
-# Color completion for some things.
-# http://linuxshellaccount.blogspot.com/2008/12/color-completion-using-zsh-modules-on.html
-zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
-
-# formatting and messages
-# http://www.masterzen.fr/2009/04/19/in-love-with-zsh-part-one/
-zstyle ':completion:*' verbose yes
-zstyle ':completion:*:descriptions' format "$fg[yellow]%B--- %d%b"
-zstyle ':completion:*:messages' format '%d'
-zstyle ':completion:*:warnings' format "$fg[red]No matches for:$reset_color %d"
-zstyle ':completion:*:corrections' format '%B%d (errors: %e)%b'
-zstyle ':completion:*' group-name ''
 
 for f in `ls $ZSH_DIR`; do
     source "$ZSH_DIR/$f"
@@ -32,7 +15,7 @@ done
 source "$ZSH_DIR/completion.zsh"
 
 autoload -U colors && colors
-
+setopt PROMPT_SUBST
 export PROMPT='
 %{$fg[red]%}%n%{$reset_color%}$(_git_color @)%m[%{$fg[green]%}%~%{$reset_color%}]
 %(?..[%?] )%# %{$fg[white]%}'
