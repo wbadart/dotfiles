aec89f0fe06de00904e6eb481ff33e8a7be93c84
Will Badart <wbadart@live.com>
2018-01-22 13:04:39 -0500

    Added VimPlug install command to vim init

--- vim/init.sh
+++ vim/init.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -sf $PWD/vimrc.vim $HOME/.vimrc || exit 1
