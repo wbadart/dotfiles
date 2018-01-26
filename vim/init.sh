a070720ec91e33b6eaefc16ca8896df8af3f5c70
Will Badart <wbadart@live.com>
2018-01-26 16:57:16 -0500

    Unpublished fixes

--- vim/init.sh
+++ vim/init.sh
@@ -3,3 +3,4 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -sf $PWD/vimrc.vim $HOME/.vimrc || exit 1
+vim -c PlugInstall -c q -c q
