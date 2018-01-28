6acd2cdb52e6bdf43e4a3b830d422f0d70951f01
Will Badart <wbadart@live.com>
2018-01-28 16:39:58 -0500

    whitespace

--- mac/init.sh
+++ mac/init.sh
@@ -3,10 +3,12 @@
 # Initialize dev tools to access git, gcc, ...
 git --version > /dev/null || xcode-select --install &
 
+
 # Set login shell to zsh
 if ! echo $SHELL | grep -q 'zsh' && which -s zsh; then
     chsh -s `which zsh`
 fi
+
 
 # Download sexy color scheme
 THEME_PATH="$HOME/Downloads/Molokai.terminal"
@@ -21,6 +23,7 @@ defaults write com.apple.Terminal "Default Window Settings" 'Molokai'
 defaults write com.apple.Terminal "Startup Window Settings" 'Molokai'
 
 defaults write ~/Library/Preferences/.GlobalPreferences AppleInterfaceStyle Dark
+
 
 exit 0
 
