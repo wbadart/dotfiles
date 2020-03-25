c492d71a6432384a12d520c6b57ef1b1147f36cc
Will Badart <badart_william@bah.com>
2020-03-25 12:44:36 -0700

    Fix quoting issue in zshrc

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -15,10 +15,10 @@ autoload edit-command-line; zle -N edit-command-line
 bindkey '^e' edit-command-line
 
 # Source the submodules
-for f in "$(ls $ZSH_DIR/*.zsh)"; do
+for f in $(ls $ZSH_DIR/*.zsh); do
     source "$f"
 done
-for f in "$(ls $ZSH_DIR/hosts/*.zsh)"; do
+for f in $(ls $ZSH_DIR/hosts/*.zsh); do
     f="$(basename "$f" .zsh)"
     if [ "$f" = "$(hostname)" -o "$f" = "$(uname)" ]; then
         source "$ZSH_DIR/hosts/$f.zsh"
