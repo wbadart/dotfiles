c406a99ebc6ef7a7d8dea8675e001574518e36e3
Will Badart <badart_william@bah.com>
2020-04-23 14:21:33 -0700

    Use TmuxlineSnapshot as tmux config

--- modules/tmux/init.sh
+++ modules/tmux/init.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
 CONFDIR="$HOME/.config/tmux"
-MAIN='tmux.conf'
 test -d "$CONFDIR" || mkdir -p "$CONFDIR"
-
-ln -sf "$PWD/$MAIN" "$HOME/.tmux.conf" || exit 1
-
-for conf in `ls *.tmux.conf | grep -v "^$MAIN$"`; do
-    ln -sf "$PWD/$conf" "$CONFDIR"
-done
+ln -sf $PWD/tmux.conf $CONFDIR/tmux.conf || exit 1
