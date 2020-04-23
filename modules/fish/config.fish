c3dc8f7b4fdd0c9d8320e07a095dc7c6f28af70b
Will Badart <badart_william@bah.com>
2020-04-23 14:11:45 -0700

    Fish updates
    I installed ruby with brew, so I added its bin to my user path. Also
    gave up on trying to have a custom PYENV_ROOT. Since the pyenv entry
    point is a bash script, it doesn't seem to get any environment variables
    from fish, and couldn't seem to get it to look at /etc/{profile,bashrc}.
    
    Also added empty fish_right_prompt function to disable the clock from
    the omf default theme. (I get the time from either the mac or tmux
    status bar.)

--- modules/fish/config.fish
+++ modules/fish/config.fish
@@ -5,11 +5,11 @@ set fish_greeting
 
 set AGDA_DIR $HOME/.config/agda
 set GNUPGHOME $HOME/.config/gnupg
-set PYENV_ROOT $HOME/.config/pyenv
+set PYENV_ROOT $HOME/.pyenv
 set STACK_ROOT $HOME/.config/stack
 set -U LESSHISTFILE '-'
 
-set -U fish_user_paths $HOME/.local/bin $HOME/.nix-profile/bin $PYENV_ROOT/bin
+set -U fish_user_paths $HOME/.local/bin $HOME/.nix-profile/bin $PYENV_ROOT/bin '/usr/local/opt/ruby/bin'
 
 pyenv init - | source
 fenv source $HOME/.nix-profile/etc/profile.d/nix.sh
