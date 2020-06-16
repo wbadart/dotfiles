08d8e0ff76202c37a6aa983e07119d9c1335ace3
Will Badart <badart_william@bah.com>
2020-06-16 14:48:05 -0700

    Remove oh my fish

--- modules/fish/config.fish
+++ modules/fish/config.fish
@@ -2,6 +2,3 @@ set fish_color_cwd brblue
 set fish_color_host brblack
 set fish_prompt_pwd_dir_length 0
 set fish_greeting
-
-set AGDA_DIR $HOME/.config/agda
-set STACK_ROOT $HOME/.config/stack
--- modules/fish/functions/fish_right_prompt.fish
+++ modules/fish/functions/fish_right_prompt.fish
@@ -1,3 +1,2 @@
 function fish_right_prompt
-    # fish_default_mode_prompt
 end
