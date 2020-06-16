80f8191389053846c08f2b4358399e9f35b23152
Will Badart <badart_william@bah.com>
2020-06-16 16:19:03 -0700

    Move config.fish to configuration.nix

--- modules/fish/config.fish
+++
@@ -1,4 +0,0 @@
-set fish_color_cwd brblue
-set fish_color_host brblack
-set fish_prompt_pwd_dir_length 0
-set fish_greeting
--- modules/nix-darwin/configuration.nix
+++ modules/nix-darwin/configuration.nix
@@ -90,7 +90,15 @@
     };
   };
 
+  programs.fish = {
+    enable = true;
+    shellInit = ''
+      set fish_color_cwd brblue
+      set fish_color_host brblack
+      set fish_prompt_pwd_dir_length 0
+      set fish_greeting
+    '';
+  };
   programs.bash.enable = true;
-  programs.fish.enable = true;
   programs.zsh.enable = true;
 }
