8b6da9f68881609ca8ecf0b15fc873991a93da51
Will Badart <will@willbadart.com>
2020-11-09 17:19:22 -0500

    Got extraPackages working

--- hosts/nix.nix
+++ hosts/nix.nix
@@ -1,19 +1,18 @@
-import ../home.nix
+{ config, pkgs, ... }:
+import /home/will/.config/nixpkgs/home.nix
 {
   username = "will";
   homeDirectory = "/home/will";
   email = "will@willbadart.com";
-  extraPackages = [  #with pkgs; [
-    # calibre
-    # entr
-    # firefox
-    # hledger
-    # htop
-    # signal-desktop
-    # tmux
+  extraPackages = with pkgs; [
+    calibre
+    firefox
+    signal-desktop
+    tmux
   ];
   extraConfig = {
     services.syncthing.enable = true;
     services.unclutter.enable = true;
   };
 }
+{ config = config; pkgs = pkgs; }
