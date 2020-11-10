7e9d7f67b2d4c8045cd1dbe5b90ff23da0acad7c
Will Badart <will@willbadart.com>
2020-11-09 19:14:24 -0500

    Add neuron service to nixos box

--- hosts/nix.nix
+++ hosts/nix.nix
@@ -14,6 +14,22 @@ import /home/will/doc/dotfiles/home.nix
   extraConfig = {
     services.syncthing.enable = true;
     services.unclutter.enable = true;
+
+    systemd.user.services.neuron = let
+      notesDir = "/home/will/doc/zettelkasten";
+      # See "Declarative Install"
+      neuron = (
+        let neuronRev = "master";
+            neuronSrc = builtins.fetchTarball "https://github.com/srid/neuron/archive/${neuronRev}.tar.gz";
+         in import neuronSrc {});
+    in {
+      Unit.Description = "Neuron zettelkasten service";
+      Install.WantedBy = [ "graphical-session.target" ];
+      Service = {
+        ExecStart = "${neuron}/bin/neuron -d ${notesDir} rib -wS";
+      };
+    };
+
   };
 }
 { pkgs = pkgs; }
