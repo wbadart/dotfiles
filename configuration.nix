d58b4745a3552ebbb5559709d0d975e8a21f26db
Will Badart <will@willbadart.com>
2021-01-27 16:17:27 -0500

    update timezone and quick polybar tweak

--- configuration.nix
+++ configuration.nix
@@ -28,7 +28,7 @@
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Set your time zone.
-  time.timeZone = "America/Denver";
+  time.timeZone = "America/New_York";
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
