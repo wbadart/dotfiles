9bbf86a6494b7ac182e7848383a5bae134ebad9c
Will Badart <will@willbadart.com>
2021-10-01 12:13:32 -0700

    Update tz and ledger file

--- modules/hledger.nix
+++ modules/hledger.nix
@@ -7,7 +7,7 @@
     sessionVariables = {
       LEDGER_FILE =
         assert config.home.sessionVariables?LEDGER_DIR;
-        "${config.home.sessionVariables.LEDGER_DIR}/SEP2021.journal";
+        "${config.home.sessionVariables.LEDGER_DIR}/OCT2021.journal";
       STOCKS_EXCLUDE = "USD|LifePath|Put|Call|ETH|BTC|BRK.B|NT_HRS|Index";
     };
   };
