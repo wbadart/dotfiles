88480d04384353ff57d4a2066b9c6c8c534fda75
Will Badart <will@willbadart.com>
2022-01-04 14:45:25 -0800

    Update LEDGER_FILE

--- modules/hledger.nix
+++ modules/hledger.nix
@@ -7,7 +7,7 @@
     sessionVariables = {
       LEDGER_FILE =
         assert config.home.sessionVariables?LEDGER_DIR;
-        "${config.home.sessionVariables.LEDGER_DIR}/NOV2021.journal";
+        "${config.home.sessionVariables.LEDGER_DIR}/JAN2022.journal";
       STOCKS_EXCLUDE = "USD|LifePath|Put|Call|ETH|BTC|BRK.B|NT_HRS|Index";
     };
   };
