cc927b43d7c7de2021fe5250767f42354abb04fd
Will Badart <will@willbadart.com>
2022-03-03 22:14:59 +0100

    Advance LEDGER_FILE

--- modules/hledger.nix
+++ modules/hledger.nix
@@ -7,7 +7,7 @@
     sessionVariables = {
       LEDGER_FILE =
         assert config.home.sessionVariables?LEDGER_DIR;
-        "${config.home.sessionVariables.LEDGER_DIR}/2022/02-2022.journal";
+        "${config.home.sessionVariables.LEDGER_DIR}/2022/03-2022.journal";
       STOCKS_EXCLUDE = "USD|LifePath|Put|Call|ETH|BTC|BRK.B|NT_HRS|Index";
     };
   };
