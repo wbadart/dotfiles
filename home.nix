1353308aa772f95204543ecdb8c980a9ea6de0a2
Will Badart <wbadart@anduril.com>
2026-04-15 11:21:33 -0700

    home: make dotfile options optional
    Just don't make the `hm` aliases if not set

--- home.nix
+++ home.nix
@@ -20,12 +20,14 @@ in
 
   options.dotfiles = {
     location = lib.mkOption {
-      type = lib.types.str;
+      type = lib.types.nullOr lib.types.str;
       description = "Path of dotfiles repo on disk";
+      default = null;
     };
     hostAttr = lib.mkOption {
-      type = lib.types.str;
+      type = lib.types.nullOr lib.types.str;
       description = "Attribute in default.nix pointing to this system's config";
+      default = null;
     };
   };
 
@@ -46,7 +48,7 @@ in
         nom = "--log-format internal-json 2> >(${lib.getExe pkgs.nix-output-monitor} --json)";
         pkgsExpr = "import (import ${location}/npins).nixpkgs {}";
       in
-      {
+      lib.mkIf (host != null && location != null) {
         hm = ''home-manager -f ${location} -A ${host} --arg pkgs "${pkgsExpr}"'';
         hmb = ''home-manager build -f ${location} -A ${host} --arg pkgs "${pkgsExpr}" ${nom}'';
         hms = ''home-manager switch -f ${location} -A ${host} --arg pkgs "${pkgsExpr}" ${nom}'';
