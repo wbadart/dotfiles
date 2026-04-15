fab7f70b252c6d006c41a18fdbc218ec4cf70da4
Will Badart <wbadart@anduril.com>
2026-04-15 11:38:16 -0700

    nvim: gate config behind `enable` option

--- hosts/mba.nix
+++ hosts/mba.nix
@@ -7,6 +7,7 @@
   home.homeDirectory = "/home/will";
   dotfiles.hostAttr = "mba";
   dotfiles.location = "$HOME/Documents/dotfiles";
+  dotfiles.neovim.enable = true;
 
   services.tailscale-systray.enable = true;
 }
--- modules/nvim/default.nix
+++ modules/nvim/default.nix
@@ -1,37 +1,41 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }:
 {
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-    extraLuaConfig = builtins.readFile ./init.lua;
-    viAlias = true;
-    vimAlias = true;
-    vimdiffAlias = true;
-    withPython3 = false;
-    withRuby = false;
-
-    plugins = with pkgs.vimPlugins; [
-      fzf-lua
-      nvim-treesitter.withAllGrammars
-      nvim-web-devicons
-      vim-fugitive
-      vim-gitgutter
-      vim-repeat
-      vim-surround
-      vim-unimpaired
-    ];
-  };
-
   imports = [
     ./modules/lua
     ./modules/misc
     ./modules/nix
   ];
 
-  # fzf-lua dependencies
-  programs.bat.enable = true;
-  programs.delta.enable = true;
-  programs.fd.enable = true;
-  programs.fzf.enable = true;
-  programs.ripgrep.enable = true;
+  options.dotfiles.neovim.enable = lib.mkEnableOption "dotfiles' neovim config";
+
+  config = lib.mkIf config.dotfiles.neovim.enable {
+    programs.neovim = {
+      enable = true;
+      defaultEditor = true;
+      extraLuaConfig = builtins.readFile ./init.lua;
+      viAlias = true;
+      vimAlias = true;
+      vimdiffAlias = true;
+      withPython3 = false;
+      withRuby = false;
+
+      plugins = with pkgs.vimPlugins; [
+        fzf-lua
+        nvim-treesitter.withAllGrammars
+        nvim-web-devicons
+        vim-fugitive
+        vim-gitgutter
+        vim-repeat
+        vim-surround
+        vim-unimpaired
+      ];
+    };
+
+    # fzf-lua dependencies
+    programs.bat.enable = true;
+    programs.delta.enable = true;
+    programs.fd.enable = true;
+    programs.fzf.enable = true;
+    programs.ripgrep.enable = true;
+  };
 }
