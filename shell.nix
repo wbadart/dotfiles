426bf44f6a337cc9fbef2e27795afb0c2dac81e0
Will Badart <wbadart@anduril.com>
2026-04-24 12:47:45 -0700

    chore: formatting, qualify npins

--- shell.nix
+++ shell.nix
@@ -1,14 +1,15 @@
-let sources = import ./npins; in
+let
+  sources = import ./npins;
+in
 {
   pkgs ? import sources.nixpkgs { },
   agenix ? import sources.agenix { inherit pkgs; },
   home-manager ? import sources.home-manager { inherit pkgs; },
-  ...
 }:
 pkgs.mkShell {
-  packages = with pkgs; [
+  packages = [
     agenix.agenix
     home-manager.home-manager
-    npins
+    pkgs.npins
   ];
 }
