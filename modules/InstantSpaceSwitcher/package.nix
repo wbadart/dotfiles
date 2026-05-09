d3fa077c89a786f31855805969a41e8738f46bf6
Will Badart <will@willbadart.com>
2026-05-09 15:55:45 -0700

    refactor(iss): use nixpkgs `fetchurl` over builtin

--- modules/InstantSpaceSwitcher/package.nix
+++ modules/InstantSpaceSwitcher/package.nix
@@ -1,12 +1,13 @@
 {
+  fetchurl,
   stdenvNoCC,
   undmg,
 }: stdenvNoCC.mkDerivation (self: {
   pname = "InstantSpaceSwitcher";
   version = "2.0";
-  src = builtins.fetchurl {
+  src = fetchurl {
     url = "https://github.com/jurplel/InstantSpaceSwitcher/releases/download/v${self.version}/InstantSpaceSwitcher-${self.version}.dmg";
-    sha256 = "sha256:1f1gcz1klid9jqmrkfl8k5ncb4icwq5diky8mz7i4pmzggccgh73";
+    hash = "sha256-48DH2Hu/XhLPr8jP2ArmLJLFbJmIupkrlqlFOsNnL7g=";
   };
   sourceRoot = ".";
   nativeBuildInputs = [ undmg ];
