5f3a30c6bfd079a1eaa146dc772fc29675a63ecf
Will Badart <will@willbadart.com>
2026-05-08 23:28:22 -0700

    chore(pi): update to latest version

--- modules/pi/package.nix
+++ modules/pi/package.nix
@@ -4,15 +4,15 @@
   pi-coding-agent,
 }:
 pi-coding-agent.overrideAttrs (final: _prev: {
-  version = "0.70.6";
+  version = "0.73.1";
   src = fetchFromGitHub {
     owner = "badlogic";
     repo = "pi-mono";
     tag = "v${final.version}";
-    hash = "sha256-XZUnKk+B9kWn51kRfMkfInYCz+5hVuWQBvgOm9PO9bo=";
+    hash = "sha256-ZcqMWghMACzEUswLujwClPF1pbwjTKzTbcYW86ZvjL4=";
   };
   npmDeps = fetchNpmDeps {
     inherit (final) src;
-    hash = "sha256-pEVIqp9rbuHFE6eqSmADmIXWAPey1VbD7qmOJwksz1o=";
+    hash = "sha256-tneAcwtTIfkcqQ8/Ch1Xa6OiOkTjJNYbH8wfhNneT/g=";
   };
 })
