8cc799d24df2fd640a7b4c66f1a14443ff233b94
Will Badart <wbadart@anduril.com>
2026-06-30 05:07:27 -0700

    chore(ssh): use new-style HM ssh settings

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -2,13 +2,13 @@
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
-    matchBlocks = {
-      "*".setEnv.TERM = "xterm-256color";
-      vultr.user = "root";
-      vultr2.user = "root";
+    settings = {
+      "*".SetEnv.TERM = "xterm-256color";
+      vultr.User = "root";
+      vultr2.User = "root";
       kindle = {
-        user = "root";
-        port = 2222;
+        User = "root";
+        Port = 2222;
       };
     };
   };
