65b926cd15f167bc9f124ddcfdce3db99de662b5
Will Badart <will@willbadart.com>
2026-07-01 00:31:59 -0700

    feat(ghostty): split creation and navigation keybindings

--- modules/ghostty.nix
+++ modules/ghostty.nix
@@ -9,6 +9,17 @@
       settings = {
         theme = "dark:Catppuccin Mocha,light:Catppuccin Latte";
         mouse-hide-while-typing = true;
+        keybind = [
+          "cmd+h=goto_split:left"
+          "cmd+j=goto_split:down"
+          "cmd+k=goto_split:up"
+          "cmd+l=goto_split:right"
+
+          "cmd+shift+h=new_split:left"
+          "cmd+shift+j=new_split:down"
+          "cmd+shift+k=new_split:up"
+          "cmd+shift+l=new_split:right"
+        ];
       };
     };
   };
