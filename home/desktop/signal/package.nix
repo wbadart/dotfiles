5eed3f77bd3f497e27c64cf03a5055dab92b20f2
Will Badart <will@willbadart.com>
2026-08-20 09:07:29 -0700

    chore(signal): update to latest

--- home/desktop/signal/package.nix
+++ home/desktop/signal/package.nix
@@ -4,11 +4,11 @@
   stdenvNoCC,
   undmg,
 }: stdenvNoCC.mkDerivation (self: {
-  version = "8.23.0";
+  version = "8.24.0";
   pname = "signal-desktop-bin";
   src = fetchurl {
     url = "https://updates.signal.org/desktop/signal-desktop-mac-universal-${self.version}.dmg";
-    hash = "sha256-v36VDOlHb3CtkYuFoi8rh+qHmtlDf3aaA5Yd2RW2XSU=";
+    hash = "sha256-I1OLnyLgjVMLIBLcIbi0Zg9coCVMW4PegdbaQ5HVJrQ=";
   };
   sourceRoot = ".";
   nativeBuildInputs = [ undmg ];
