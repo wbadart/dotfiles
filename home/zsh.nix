03dc7030c2c1a09717402cc5af228f6a9b2d21d7
Will Badart <wbadart@anduril.com>
2026-09-08 14:43:59 -0700

    feat(zsh): add option to enable profiling
    Based on:
    
    - https://stackoverflow.com/questions/4351244/can-i-profile-my-zshrc-zshenv
    - https://esham.io/2018/02/zsh-profiling

--- home/zsh.nix
+++ home/zsh.nix
@@ -3,68 +3,110 @@ let
   ifSecrets = x: lib.mkIf config.wb.secrets.enable x;
 in
 {
-  programs.zsh = {
-    enable = true;
-    autocd = true;
-    autosuggestion.enable = true;
-    defaultKeymap = "viins";
-    dotDir = "${config.xdg.configHome}/zsh";
-    enableCompletion = true;
-    enableVteIntegration = true;
-    envExtra = ifSecrets ". ${config.age.secrets.env.path}";
-    history.append = true;
-    history.share = false;
-    syntaxHighlighting.enable = true;
-  };
-
-  home.sessionPath = [
-    "$HOME/.local/bin"
-  ];
-
-  home.shellAliases = {
-    l = "eza -l";
-    ll = "eza -la";
-    ls = "eza";
-    lt = "eza --tree";
-    cat = "bat";
-    da = "direnv allow";
-    dr = "direnv reload";
-  };
-
-  programs.eza = {
-    enable = true;
-    git = true;
-    icons = "auto";
-    extraOptions = [
-      "--group-directories-first"
-    ];
-  };
-
-  programs.nix-your-shell = {
-    enable = true;
-    nix-output-monitor.enable = true;
-  };
-
-  programs.direnv = {
-    enable = true;
-    nix-direnv.enable = true;
-    config.global = {
-      hide_env_diff = true;
-      strict_env = true;
-      warn_timeout = 0;
+  options.wb.zsh.doProfile =
+    let
+      inherit (lib) types;
+    in
+    lib.mkOption {
+      type = types.bool;
+      default = false;
+      example = true;
+      description = "Whether to enable zsh startup profiling";
     };
-  };
 
-  programs.bat.enable = true;
-  programs.fd.enable = true;
-  programs.fzf.enable = true;
-  programs.jq.enable = true;
-  programs.jqp.enable = true;
-  programs.ripgrep.enable = true;
-  programs.starship.enable = true;
-  programs.zoxide.enable = true;
+  config =
+    let
+      cfg = config.wb.zsh.doProfile;
+    in
+    {
+      programs.zsh = {
+        enable = true;
+        autocd = true;
+        autosuggestion.enable = true;
+        defaultKeymap = "viins";
+        dotDir = "${config.xdg.configHome}/zsh";
+        enableCompletion = true;
+        enableVteIntegration = true;
+        envExtra = ifSecrets ". ${config.age.secrets.env.path}";
+        history.append = true;
+        history.share = false;
+        syntaxHighlighting.enable = true;
 
-  age.secrets = ifSecrets {
-    env.file = ../secrets/env.age;
-  };
+        # https://esham.io/2018/02/zsh-profiling
+        initContent = lib.mkIf cfg.doProfile (
+          let
+            early = ''
+              zmodload zsh/datetime
+              setopt PROMPT_SUBST
+              PS4='+$EPOCHREALTIME %N:%i> '
+
+              logfile=$(mktemp zsh_profile.XXXXXXXX)
+              echo "Logging to $logfile"
+              exec 3>&2 2>$logfile
+
+              setopt XTRACE
+            '';
+            late = ''
+              unsetopt XTRACE
+              exec 2>&3 3>&-
+            '';
+          in
+          lib.mkMerge [
+            (lib.mkOrder 10 early)
+            (lib.mkOrder 9999 late)
+          ]
+        );
+      };
+
+      home.sessionPath = [
+        "$HOME/.local/bin"
+      ];
+
+      home.shellAliases = {
+        l = "eza -l";
+        ll = "eza -la";
+        ls = "eza";
+        lt = "eza --tree";
+        cat = "bat";
+        da = "direnv allow";
+        dr = "direnv reload";
+      };
+
+      programs.eza = {
+        enable = true;
+        git = true;
+        icons = "auto";
+        extraOptions = [
+          "--group-directories-first"
+        ];
+      };
+
+      programs.nix-your-shell = {
+        enable = true;
+        nix-output-monitor.enable = true;
+      };
+
+      programs.direnv = {
+        enable = true;
+        nix-direnv.enable = true;
+        config.global = {
+          hide_env_diff = true;
+          strict_env = true;
+          warn_timeout = 0;
+        };
+      };
+
+      programs.bat.enable = true;
+      programs.fd.enable = true;
+      programs.fzf.enable = true;
+      programs.jq.enable = true;
+      programs.jqp.enable = true;
+      programs.ripgrep.enable = true;
+      programs.starship.enable = true;
+      programs.zoxide.enable = true;
+
+      age.secrets = ifSecrets {
+        env.file = ../secrets/env.age;
+      };
+    };
 }
