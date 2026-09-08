af96b625cfb6a0b51d75f9b2b5827b447b512509
Will Badart <wbadart@anduril.com>
2026-09-08 14:54:08 -0700

    fix(zsh): put zsh.wb options in programs namespace

--- home/zsh.nix
+++ home/zsh.nix
@@ -3,7 +3,7 @@ let
   ifSecrets = x: lib.mkIf config.wb.secrets.enable x;
 in
 {
-  options.zsh.wb.doProfile =
+  options.programs.zsh.wb.doProfile =
     let
       inherit (lib) types;
     in
@@ -16,7 +16,7 @@ in
 
   config =
     let
-      cfg = config.zsh.wb;
+      cfg = config.programs.zsh.wb;
     in
     {
       programs.zsh = {
